838932359589950db57865bbe780575363e1b6d7
aditya041997 <kashyap.singh52@gmail.com>
2021-05-10 16:00:54 +0530

    Modified some files

---
+++ A.js
@@ -0,0 +1,15 @@
+let tony={
+    name:'Tony',
+    lastName:'Stark',
+    friends:['Robert','Bruce','Peter'],
+    age:45,
+    address:{
+        state:"NewYork",
+        city:"Long Island"
+    },
+    saysHi:function fn(){
+        console.log('Iron Man says hello');
+        return "Part of Journey is the end";
+    }
+}
+console.log(tony);
